df9f255299dd5d36c0b33babdd579a4dd6601afc
Ronal Cucariano <ronalcucariano@gmail.com>
2025-11-20 01:46:38 -0500

    Chore: se ajustan errores de config por versiones nuevas y se avanza en la pantalla de Home

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDarkMode } from "../components/DarkModeContext";
-import homeimg from "../assets/images/roomrent.png";
+import cityimg from "../assets/images/city.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -9,16 +9,36 @@ const Home = () => {
     AOS.init({
       offset: 200,
       duration: 800,
-      easing: "ease-out-sine",
+      easing: "ease-in-sine",
       delay: 100,
     });
-  }, []);
+  }, [])
 
   const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <>
-      <div className={`${darkMode ? 'dark bg-black' : 'light bg-white'}`}>
-        
+      <div className={`${darkMode ? "dark bg-black" : "light bg-white"}`}>
+        <section
+          id="home"
+          className="w-[95%] h-[600px] m-auto bg-cover bg-center rounded-xl flex justify-center flex-col items-start lg:px-28 px-10 gap-7 z-20"
+          style={{ backgroundImage: `url(${cityimg})` }}
+        >
+          <h1
+            data-aos="zoom-in"
+            className="text-6xl font-semibold text-white lg:pr-[500px] pr-0 lg:leading-[70px] leading-[60px]"
+          >
+            Encuentra tu proximo hogar en Bogotá
+          </h1>
+          <p
+            data-aos="zoom-in"
+            className="text-white text-xl lg:pr-[500px] pr-0"
+          >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            iusto fugiat! Doloremque earum, eum, obcaecati nisi sunt ducimus
+            explicabo cupiditate eos quidem officiis est molestiae enim! Officia
+            cumque dolore totam.
+          </p>
+        </section>
       </div>
     </>
   );
