9e423f0f530d8ec41134bb75e19c049111bd0ba7
Ronal David Cucariano_ ContactRcucarianoBusiness <ronalcucariano@gmail.com>
2025-11-24 21:19:54 -0500

    chore: Se crea apartado de popular area y se inicia con propiedades

--- src/sections/PopularAreas.jsx
+++ src/sections/PopularAreas.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect } from 'react'
-import { useDarkMode } from '../components/DarkModeContext'
-import area1 from '../assets/images/House.jpg'
-import area2 from '../assets/images/Apartment.jpg'
-import area3 from '../assets/images/Room.jpeg'
+import React, { useEffect } from "react";
+import { useDarkMode } from "../components/DarkModeContext";
+import area1 from "../assets/images/House.jpg";
+import area2 from "../assets/images/Apartment.jpg";
+import area3 from "../assets/images/Room.jpeg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const PopularAreas = () => {
-
   useEffect(() => {
     AOS.init({
       offset: 200,
@@ -15,29 +14,98 @@ const PopularAreas = () => {
       easing: "ease-in-sine",
       delay: 100,
     });
-  }, [])
+  }, []);
 
   const { darkMode, toggleDarkMode } = useDarkMode();
 
   return (
     <div className={`${darkMode ? "dark bg-black" : "light bg-transparent"}`}>
-      <section className={`${darkMode ? "dark bg-gray-800" : "light bg-red-100"} lg:w-[90%] w-full h-fit m-auto bg-cover bg-center rounded-xl flex justify-center flex-col items-center lg:px-20 px-6 py-20 gap-20`}>
-        <div id='top' className='w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8'>
+      <section
+        className={`${
+          darkMode ? "dark bg-gray-800" : "light bg-red-100"
+        } lg:w-[90%] w-full h-fit m-auto bg-cover bg-center rounded-xl flex justify-center flex-col items-center lg:px-20 px-6 py-20 gap-20`}
+      >
+        <div
+          id="top"
+          className="w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8"
+        >
           <div>
-            <h1 data-aos="zoom-in" className='text-red-500 dark:text-white'>TIPOS DE ARRIENDO</h1>
-            <h1 data-aos="zoom-in" className='text-black text-[40px] font-semibold leading-10 mt-4 dark:text-white'>Explora todos los <br></br> inmuebles disponibles</h1>
+            <h1 data-aos="zoom-in" className="text-red-500 dark:text-white">
+              TIPOS DE ARRIENDO
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              className="text-black text-[40px] font-semibold leading-10 mt-4 dark:text-white"
+            >
+              Explora todos los <br></br> inmuebles disponibles
+            </h1>
           </div>
 
-          <div className='grid lg:grid-cols-3 col-span-2 grid-cols-1 justify-center items-center gap-6'>
-  <div data-aos="zoom-in" data-aos-delay="400" style={{ backgroundImage:`url(${area1})` }} className='h-[200px] bg-cover bg-center rounded-xl transform transition-transform duration-500 hover:scale-110'></div>
-  <div data-aos="zoom-in" data-aos-delay="500" style={{ backgroundImage:`url(${area2})` }} className='h-[200px] bg-cover bg-center rounded-xl transform transition-transform duration-500 hover:scale-110'></div>
-  <div data-aos="zoom-in" data-aos-delay="600" style={{ backgroundImage:`url(${area3})` }} className='h-[200px] bg-cover bg-center rounded-xl transform transition-transform duration-500 hover:scale-110'></div>
-</div>
-      
+          <div className="grid lg:grid-cols-3 col-span-2 grid-cols-1 justify-center items-center gap-6">
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="400"
+              style={{ backgroundImage: `url(${area1})` }}
+              className="h-[300px] bg-cover bg-center rounded-xl transform transition-transform duration-500 hover:scale-110"
+            ></div>
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="500"
+              style={{ backgroundImage: `url(${area2})` }}
+              className="h-[300px] bg-cover bg-center rounded-xl transform transition-transform duration-500 hover:scale-110"
+            ></div>
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="600"
+              style={{ backgroundImage: `url(${area3})` }}
+              className="h-[300px] bg-cover bg-center rounded-xl transform transition-transform duration-500 hover:scale-110"
+            ></div>
+          </div>
+        </div>
+        <div
+          id="bottom"
+          className="w-full grid lg:grid-cols-3 grid-cols-1 lg:justify-center justify-start items-center gap-6"
+        >
+          <div
+            data-aos="zoom-in"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center gap-8 w-full"
+          >
+            <h1 className="text-black text-7xl font-semibold dark:text-white">
+              5K
+            </h1>
+            <h1>
+              PROPIEDADES <br /> DISPONIBLES
+            </h1>
+          </div>
+          <div
+            data-aos="zoom-in"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center gap-8 w-full"
+          >
+            <h1 className="text-black text-7xl font-semibold dark:text-white">
+              +1K
+            </h1>
+            <h1>
+              ARRENDADORES HAN <br /> CONFIADO EN NOSOTROS
+            </h1>
+          </div>
+          <div
+            data-aos="zoom-in"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center gap-8 w-full"
+          >
+            <h1 className="text-black text-7xl font-semibold dark:text-white">
+              +800
+            </h1>
+            <h1>
+              ARRENDATARIOS HAN <br /> ENCONTRADO SU HOGAR <br /> CON NOSOTROS
+            </h1>
+          </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default PopularAreas
+export default PopularAreas;
--- src/sections/Properties.jsx
+++ src/sections/Properties.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDarkMode } from "../components/DarkModeContext";
+/*import { property } from '../components/Export'*/
+import {
+  FaBath,
+  FaShareAlt,
+  FaBed,
+  FaUserCircle,
+  FaPlus,
+  FaMapMarkerAlt,
+  FaVideo,
+  FaCamera,
+} from "react-icons/fa";
+import { MdSpaceDashboard } from "react-icons/md";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Properties = () => {
-  return (
-    <div>Properties</div>
-  )
-}
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  }, []);
 
-export default Properties
+  const { darkMode, toggleDarkMode } = useDarkMode();
+  return (
+    <div className={`${darkMode ? "dark bg-black" : "light bg-transparent"}`}>
+      <section
+        id="Properties"
+        className="lg:w-[90%] m-auto lg:px-20 px-6 py-20 w-full flex flex-col justify-center items-start gap-10"
+      >
+        <div className="flex flex-col justify-center items-start gap-4">
+          <h1 data-aos="zoom-in" className='text-red-500 dark:text-white'>PROPIEDADES</h1>
+          <h1 data-aos="zoom-in" className='text-black text-4xl font-semibold dark:text-white'>Explora los inmuebles disponibles</h1>
+        </div>
+
+        {/* Property grid start */}
+
+        <div id='grid-box' className='w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8'>
+{
+  Property.map((item, index) => (
+    <div data-aos="zoom-in" data-aos-delay="200" key={index} className='bg-white dark:bg-gray-800 rounded-xl w-full'>
+      <div id="image-box" className='bg-cover bg-center h-[250px] rounded-xl p-4 flex flex-col justify-between items-end' style={{backgroundImage:
+        `url(${item.images})` }}>
+
+      </div>
+    </div>
+}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Properties;
