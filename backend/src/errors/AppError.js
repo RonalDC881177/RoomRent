9697677dfe831f6687a8682d9c079bcacec8d364
Ronal <ronalcucariano@gmail.com>
2026-05-07 23:26:04 -0500

    Se organiza el manejo de errores para la opcion de create user

--- backend/src/errors/AppError.js
+++ backend/src/errors/AppError.js
@@ -1,3 +1,23 @@
 /***
  * Errores personalizados para la aplicacion
+ * class AppError extends Error: crea una clase personalizada basada en el sistema de errores
+ * super(message): Llama al contructor de error.
+ * this.statusCode = statusCode: Guarda el codigo HTTP del error.
+ * this.status = `${statusCode}`.startsWith('4'): determina si el error es del cliente codigos 4xx o si es del servidor codigos 5xx.
+ * this.isOperational = true: Marca el errores controlados.
+ * Error.captureStackTrace(this, this.constructor): Captura la pila de llamadas para ayudar en la depuración.
  */
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+        this.isOperational = true;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default AppError;
--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,4 +1,17 @@
 /***
  * Wrapper automático para async/await.
  * Permite manejar errores de forma centralizada sin tener que usar try/catch en cada funcion.
+ * 
+ * asyncHandler(): captura automaticamente los errores async y los manda a next(error).
+ * return (req, res, next): devuelve un middleware valido.
+ * Promise.resolve(fn(req, res, next)): Convierte el resultado en una Promise segura.
+ * .catch(next): Si la promesa se rechaza, el error se pasa al siguiente middleware de manejo de errores.
  */
+
+const asyncHandler = (fn) =>{
+    return (req, res, next) =>{
+        Promise.resolve(fn(req, res, next)).catch(next)
+    };
+};
+
+export default asyncHandler;
